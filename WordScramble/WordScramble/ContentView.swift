08f7b21cf2c2cb52a19710c89e1019be04d8284e
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-14 15:08:50 +0900

    Disable autocapitalization

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             List {
                 Section {
                     TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
                 }
                 Section {
                     ForEach(usedWords, id: \.self) { word in
