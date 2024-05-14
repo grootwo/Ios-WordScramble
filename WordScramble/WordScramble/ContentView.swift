76cdb75339b421e0da927207e6210d563aa00c96
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-14 15:03:39 +0900

    Add word input to the list

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,37 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    let cats = ["cheese", "tabby", "tuxedo", "chaos"]
+    @State private var usedWords = [String]()
+    @State private var newWord = ""
+    @State private var rootWord = ""
     var body: some View {
-        List{
-            Section("cats") {
-                ForEach(cats, id: \.self) {
-                    Text($0)
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        Text(word)
+                    }
                 }
             }
-            Section("numbers") {
-                Text("1")
-                Text("2")
-                Text("3")
-            }
         }
-        .listStyle(.grouped)
+        .navigationTitle(rootWord)
+        .onSubmit(addWord)
     }
     
-    func testBundles() {
-        if let fileUrl = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileUrl) {
-                // file is loaded into a string
-            }
-        }
-    }
-    func testStrings() {
-        let word = "swift"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
-        print(allGood)
+    func addWord() {
+        let word = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard word.count > 0 else { return }
+        usedWords.insert(word, at: 0)
+        newWord = ""
     }
 }
 
